2dec77c45c525e0dd79ab5dbe8403b28ae295224
Leonardo Brito <lbrito@gmail.com>
2014-06-22 19:06:28 -0300

    Implementado RLTP usando LRLBP e URLBP

--- ltp.m
+++ ltp.m
@@ -7,15 +7,26 @@
 %      limiar LTP  T=9
 %
 %=================================================
-function h = ltp(im, blockdim, bin, robust)
-      llbp = lbp(im, blockdim, "llbp", bin);
-      ulbp = lbp(im, blockdim, "ulbp", bin);
+function h = ltp(im, block, type, bin)
+      llbp = lbp(im, block, "llbp", bin);
+      ulbp = lbp(im, block, "ulbp", bin);
       
-      if (robust)
-            urlbp = max(
-      endif;
-      
-      h = ltp_hist = [llbp{2}, ulbp{2}];
+      switch(type)
+            case "rltp"
+                  lrlbp = min(ulbp{1}, llbp{1});
+                  urlbp = max(ulbp{1}, llbp{1});
+                  
+                  hl = zeros([1,59]);
+                  hu = zeros([1,59]);
+                  
+                  for x=1:numel(lrlbp)
+                        hl(bin(lrlbp(x)+1)) += 1;
+                        hu(bin(urlbp(x)+1)) += 1;
+                  end;
+                  h = ltp_hist = [hl,hu];
+            case "ltp"
+                  h = ltp_hist = [llbp{2}, ulbp{2}];
+      endswitch;
       bar(ltp_hist,1.0);      %produz histograma
       colormap(hsv(12));
 endfunction;

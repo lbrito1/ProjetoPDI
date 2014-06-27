e3da367a0fdb001645fc48625df7b41de965ccc9
Leonardo Brito <lbrito@gmail.com>
2014-06-26 22:46:08 -0300

    consertado SVM

--- run_svm.m
+++ run_svm.m
@@ -7,14 +7,14 @@ function svm = run_svm(treinamento, tipo)
       end;
       bin = loadbin;
 
-      for n=1:10
+      for n=1:100
             n
             %features{n} =  image_histogram(imgs{n},bin,tipo);
-            ft(n,:) =  image_histogram(imgs{n},bin,"ltp");
+            ft(n,:) = image_histogram(imgs{n},bin,"ltp");
       end;
       
 
       %group = [ones(50, 1), zeros(50, 1)];
-      group = [zeros(1,5) ones(1,5)]';
-      svm = svmtrain(ft,group);
+      group = [zeros(1,50) ones(1,50)]';
+      svm = svmtrain(group,ft);
 endfunction;
